3762f70b994c7a13a3be7bed46aa72fdb38ea1b9
Himanshu Shukla <133512069+HimanshuTechHelps@users.noreply.github.com>
2023-08-01 11:04:24 +0530

    sadas

--- pages/shop/index.js
+++ pages/shop/index.js
@@ -1059,7 +1059,7 @@ const SingleVendor = ({ property }) => {
                                       className="text-body-lead color-gray-700  text-center"
                                       style={{ lineHeight: "25px" }}
                                     >
-                                      {property?.title}
+                                      abcd{property?.title}
                                     </h3>
                                   </a>
                                 </Link>
