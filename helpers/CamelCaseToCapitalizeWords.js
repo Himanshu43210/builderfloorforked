0d2c2d15d243128d6f0d82289e995ba398b30a9c
HimanshuTechHelps <133512069+HimanshuTechHelps@users.noreply.github.com>
2023-09-06 06:16:55 +0000

    asdsad

--- helpers/CamelCaseToCapitalizeWords.js
+++
@@ -1,5 +0,0 @@
-export function getCapitalizeWords(text) {
-  const result = text?.replace(/([A-Z])/g, " $1");
-  const finalResult = result?.charAt(0).toUpperCase() + result?.slice(1);
-  return finalResult;
-}
