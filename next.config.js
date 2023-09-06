600cd058a245b3b2312ea9108b0ed673dffaae7f
HimanshuTechHelps <133512069+HimanshuTechHelps@users.noreply.github.com>
2023-09-06 11:05:29 +0530

    commit

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ["testerp1apis.nextsolutions.in"],
-  },
-};
-
-module.exports = nextConfig;
